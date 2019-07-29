ab0344160e1d37cb9d06210dc181362d8fc74c72
Artem Kurylev <artembonapart@gmail.com>
2019-07-29 15:05:46 +0400

    Created Field and game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour{
+
+    private bool isGameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (!isGameOver)
+        {
+            SpawnFigures();
+        }
+    }
+
+    void SpawnFigures()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
