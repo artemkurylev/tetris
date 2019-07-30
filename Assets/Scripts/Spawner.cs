d351afea58908475ace1cfe19ba2537134b7ef81
Artem Kurylev <artembonapart@gmail.com>
2019-07-30 13:19:07 +0400

    Creates a random Figure on start

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] figures;
+
+
+    void Spawn()
+    {
+        int index = Random.Range(0, figures.Length - 1);
+
+        Instantiate(figures[index], transform.position, Quaternion.identity);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
